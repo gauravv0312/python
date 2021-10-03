b83fcf61475354e6dd372c50ef998d4099791d10
Gautam Sharma <gautamsharma88540@gmail.com>
2021-10-03 17:58:14 +0530

    Update Calculator.py

--- python program/calculator.py
+++ python program/calculator.py
@@ -1,8 +1,8 @@
 sum = 0
 while (True):
-    userInput = input("Enter the price or press q to quit:\n");
-    if (userInput != 'q'):
-        sum = sum + int(userInput)
+    userInputNumber = input("Enter the price or press q to quit:\n");
+    if (userInputNumber != 'q'):
+        sum = sum + int(userInputNumber)
         print(f" total price:{sum}")
     else:
         print(f"your Total Amount is{sum}.Thanks You Much")
